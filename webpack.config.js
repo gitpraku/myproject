9ea75238919d99030a60a3b21badc29bfc0ed2c4
Prakash Kumar <prakashk885@gmail.com>
2018-09-01 16:17:44 +0530

    add sass

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+
+module.exports = {
+  entry: "./src/index.js", // string | object | array
+  // Here the application starts executing
+  // and webpack starts bundling
+  output: {
+  // options related to how webpack emits results
+    path: path.resolve(__dirname, "dist"), // string
+    // the target directory for all output files
+    // must be an absolute path (use the Node.js path module)
+    filename: "bundle.js", // string
+    // the filename template for entry chunks
+  },
+  module: {
+    rules: [{
+        test: /\.scss$/,
+          use: [{
+            loader: "style-loader"
+          }, {
+            loader: "css-loader" 
+          }, {
+            loader: "sass-loader"
+          }]
+    },
+    {
+      test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+    }]
+  }
